66e6fb790d5bdafab5a695b7e722e50d773cbeb4
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2023-07-02 23:31:52 +0200

    map-anisotropy removed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 });
 app.use("/", dalleRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
